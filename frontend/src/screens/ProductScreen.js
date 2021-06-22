7c6c7e7c0501203b3ce11b8a5152059a40e1cc07
Rohan Sadhukhan <rohansadhukhan9@gmail.com>
2021-06-22 15:18:55 +0530

    route added

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductScreen = () => {
+    return (
+        <div>
+            this is product
+            
+        </div>
+    )
+}
+
+export default ProductScreen
