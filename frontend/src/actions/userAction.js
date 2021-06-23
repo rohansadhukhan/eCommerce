4208383104cd1b74bfea8f31b324554abe2132bc
Rohan Sadhukhan <rohansadhukhan9@gmail.com>
2021-06-23 16:12:29 +0530

    login reducer and action added

---
+++ frontend/src/actions/userAction.js
@@ -0,0 +1,31 @@
+import { 
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAIL,
+    USER_LOGOUT
+} from "../constants/user.js"
+import axios from 'axios'
+
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST
+        })
+        const config = {
+            'Content-Type': 'application/json'
+        }
+        const {data} = axios.post('/api/users/login', {email, password}, config)
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    } catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        })       
+    }
+}
