a424a9057ff86d7c963acfeeadf3b541be67ef75
Rohan Sadhukhan <rohansadhukhan9@gmail.com>
2021-06-24 01:15:26 +0530

    shipping screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
           <Route path='/login' component={LoginScreen} exact />
           <Route path='/register' component={RegisterScreen} exact />
           <Route path='/profile' component={ProfileScreen} exact />
+          <Route path='/shipping' component={ShippingScreen} exact />
         </Container>
       </main>
       <Footer />
