3acc4c1811cd115b1c0a4919b15164733674463f
Rohan Sadhukhan <rohansadhukhan9@gmail.com>
2021-06-24 01:41:58 +0530

    payment screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
           <Route path='/register' component={RegisterScreen} exact />
           <Route path='/profile' component={ProfileScreen} exact />
           <Route path='/shipping' component={ShippingScreen} exact />
+          <Route path='/payment' component={PaymentScreen} exact />
         </Container>
       </main>
       <Footer />
