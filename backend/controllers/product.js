b8ac711138b07dea1a96a01257a6489fb7bb4e15
Rohan Sadhukhan <rohansadhukhan9@gmail.com>
2021-06-26 14:44:20 +0530

    create and update product router and controller added

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -28,11 +28,53 @@ const deleteProduct = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
     if (product) {
         await product.remove()
-        res.json({message: 'Product removed'})
+        res.json({ message: 'Product removed' })
     } else {
         res.status(404)
         throw new Error('Product not found')
     }
 })
 
-export { getProducts, getProductById, deleteProduct }
+// @desc        create a product
+// @route       POST api/products/
+// @access      private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+    const product = new Product({
+        name: 'new',
+        price: 0,
+        user: req.user._id,
+        image: '/images/new.jpg',
+        brand: 'new',
+        category: 'new',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'new'
+    })
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
+})
+
+// @desc        update a product
+// @route       PUT api/products/:id
+// @access      private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+    const { name, price, image, brand, category, countInStock, description } = req.body
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        product.name = name
+        product.price = price
+        product.description = description
+        product.image = image
+        product.brand = brand
+        product.category = category
+        product.countInStock = countInStock
+
+        const updatedProduct = await product.save()
+        res.json(updatedProduct)
+    } else {
+        res.status(404)
+        throw new Error('product not found')
+    }
+})
+
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct }
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import { protect, admin } from '../middlewares/auth.js'
-import { getProducts, getProductById, deleteProduct } from '../controllers/product.js'
+import { getProducts, getProductById, deleteProduct, createProduct, updateProduct } from '../controllers/product.js'
 const router = express.Router()
 
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
+router.route('/')
+    .get(getProducts)
+    .post(protect, admin, createProduct)
+router.route('/:id')
+    .get(getProductById)
+    .put(protect, admin, updateProduct)
     .delete(protect, admin, deleteProduct)
 
 export default router
