3b656813bc9bae822f375c2c6a2ef2dfbe4cf1c2
Rohan Sadhukhan <rohansadhukhan9@gmail.com>
2021-06-26 09:04:11 +0530

    admin middleware added

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import asyncHandler from 'express-async-handler'
 import User from '../models/User.js'
+import e from 'express'
 
 const protect = asyncHandler(async (req, res, next) => {
     let token
@@ -23,4 +24,13 @@ const protect = asyncHandler(async (req, res, next) => {
     }
 })
 
-export { protect }
+const admin = asyncHandler(async (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next()
+    } else {
+        res.status(401)
+        throw new Error ('Not Authorized')
+    }
+})
+
+export { protect, admin }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import { protect } from '../middlewares/auth.js'
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/user.js'
+import { protect, admin } from '../middlewares/auth.js'
+import { authUser, getUserProfile, getUsers, registerUser, updateUserProfile } from '../controllers/user.js'
 const router = express.Router()
 
-router.route('/').post(registerUser)
+router.route('/')
+    .post(registerUser)
+    .get(protect, admin, getUsers)
+
 router.route('/login').post(authUser)
+
 router.route('/profile')
     .get(protect, getUserProfile)
     .put(protect, updateUserProfile)
